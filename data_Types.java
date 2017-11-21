0dbd361ed15af139cc3ba66044f1a530bd25731f
anish_shan <anish@gmail.com>
2017-11-21 20:26:46 +0530

    second update

--- data_Types.java
+++ data_Types.java
@@ -1,23 +1,9 @@
 //Java_code/data_Types
 
 class data_Types{
-     public static void main(String args[]){
-        //byte byteVar = 5;
-        //short shortVar = 20;
-        //int intVar = 30;
-        //long longVar = 60;
-       // float floatVar = 20;
-       // double doubleVar = 20.123;
-       // boolean booleanVar = true;
-       // char charVar ='W';
-    
-        System.out.println(" HELLO WORLD ");
-        //System.out.println("Value Of short Variable is " + shortVar);
-       // System.out.println("Value Of int Variable is " + intVar);
-       // System.out.println("Value Of long Variable is " + longVar);
-       // System.out.println("Value Of float Variable is " + floatVar);
-       // System.out.println("Value Of double Variable is " + doubleVar);
-       // System.out.println("Value Of boolean Variable is " + booleanVar);
-       // System.out.println("Value Of char Variable is " + charVar);
-     }
+    public static void main(String args[])
+    {
+      System.out.println(" HELLO WORLD ");
+      System.out.println("Second Line ")
+    }
  }
